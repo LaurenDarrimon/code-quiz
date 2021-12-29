bbda186276ca24072614f65ee65121217d85d7a4
LaurenDarrimon <laurendarrimon@gmail.com>
2021-12-28 18:07:33 -0800

    write loop to display questions and answer buttons

--- assets/script.js
+++ assets/script.js
@@ -6,58 +6,81 @@ let codeQuiz = {
     //create nested array object that will hold question and answer sets. 
     questionsAndAnswers : [
         {
-            question: "",
-            options:["a", "b", "c", "d"],
-            rightAnswer: 1
+            question: "Commonly used data types do NOT include ___________. ",
+            options:["strings", "alerts", "booleans", "numbers"],
+            rightAnswer: 2
         }, 
         {
-            question: "",
-            options:["a", "b", "c", "d"],
-            rightAnswer: 1
+            question: "The condition for an if/else statement is enclosed within ___________. ",
+            options:["parentheses", "curly brackets", "square brackets", "single quotes"],
+            rightAnswer: 0
         }, 
         {
-            question: "",
-            options:["a", "b", "c", "d"],
-            rightAnswer: 1
+            question: "Arrays in Javascript can be used to store ________. ",
+            options:["numbers and strings", "booleans", "other arrays", "all of the above"],
+            rightAnswer: 3
         }, 
         {
-            question: "",
-            options:["a", "b", "c", "d"],
-            rightAnswer: 1
+            question: "What are the first two words programmers learn to code?",
+            options:["Ha ha!", "Hello, Friend!", "Hello, World!", "The Force"],
+            rightAnswer: 2
+        }, 
+        {
+            question: "String values must be enclosed within __________ ?",
+            options:[" parentheses", " sqaure brackets  ", "  curly brackets  ", " quotation marks  "],
+            rightAnswer: 3
         }, 
     ],
+};
         
 
     //initialize variables needed globally
-    timer: 100,
-    questionNumber:0,
-    score:0,
+    let timer = 100;
+    let questionNumber  = 1;
+    let score = 0;
 
     //declare overarching function that will contain smaller nested functions that are each part of gameplay
-    playGame: function(){
+    function playGame(){
         console.log("play game");
-        
-    }, 
+
+        //DISPLAY THE QUESTION
+        // find and display question number 
+        document.getElementById("question-number").innerHTML =  questionNumber;
+        //point to the question feild
+        let questionField = document.getElementById("question-field");
+        questionField.innerHTML = codeQuiz.questionsAndAnswers[questionNumber - 1].question;
+
+        //DISPLAY ANSWER BUTTONS
+        var answerSection = document.getElementById("answer-section"); //find the answer button section
+        answerSection.innerHTML = ""; //clear the old stuff
+
+        // a little loop that will append a button for each possible answer option in the array
+        for (let i = 0; i < codeQuiz.questionsAndAnswers.length - 1; i++ ){
+            console.log(codeQuiz.questionsAndAnswers[questionNumber-1].options[i]);
+
+            let answerButton = document.createElement("BUTTON");   
+            answerButton.innerHTML = codeQuiz.questionsAndAnswers[questionNumber-1].options[i];                   
+            answerSection.appendChild(answerButton);
+
+            answerButton.addEventListener("click", checkAnswer);
+        };
+    } 
 
 
-    //declare smaller nested functions
-    displayQuestion: function(){
-        console.log("display");
-        
-
-    }, 
-
-    checkAnswer: function(){
+    function checkAnswer(choice){
         console.log("check answer");
 
-    }, 
+       
 
-    updateScore: function(){
+
+    };
+
+    function updateScore() {
         console.log("update score" );
 
-    }, 
+    };
 
-};
+
 
 //when the page loads ask the user if they'd like to play the game. When they say yes, then run the code quiz. 
 
@@ -66,5 +89,5 @@ let startGameButton = document.querySelector("#start");
 
 
 //when start button is clicked, the code to play the game will run
-startGameButton.addEventListener("click", codeQuiz.playGame);
+startGameButton.addEventListener("click", playGame);
 
