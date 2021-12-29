6d7a92aad6fdb2a6402ecc046aaeafc667899bcb
LaurenDarrimon <laurendarrimon@gmail.com>
2021-12-28 22:36:14 -0800

    write javascript for code quiz questions display, answer display, and answer comparision

--- assets/script.js
+++ assets/script.js
@@ -8,36 +8,38 @@ let codeQuiz = {
         {
             question: "Commonly used data types do NOT include ___________. ",
             options:["strings", "alerts", "booleans", "numbers"],
-            rightAnswer: 2
+            rightAnswer: "numbers",
         }, 
         {
             question: "The condition for an if/else statement is enclosed within ___________. ",
             options:["parentheses", "curly brackets", "square brackets", "single quotes"],
-            rightAnswer: 0
+            rightAnswer: "parentheses",
         }, 
         {
             question: "Arrays in Javascript can be used to store ________. ",
             options:["numbers and strings", "booleans", "other arrays", "all of the above"],
-            rightAnswer: 3
+            rightAnswer: "all of the above",
         }, 
         {
             question: "What are the first two words programmers learn to code?",
             options:["Ha ha!", "Hello, Friend!", "Hello, World!", "The Force"],
-            rightAnswer: 2
+            rightAnswer: "Hello, World!",
         }, 
         {
             question: "String values must be enclosed within __________ ?",
-            options:[" parentheses", " sqaure brackets  ", "  curly brackets  ", " quotation marks  "],
-            rightAnswer: 3
+            options:["parentheses", "sqaure brackets", "curly brackets", "quotation marks"],
+            rightAnswer: "quotation marks",
         }, 
     ],
 };
         
-
     //initialize variables needed globally
     let timer = 100;
     let questionNumber  = 1;
     let score = 0;
+    let questionField = document.getElementById("question-field"); //point to the question feild
+    let answerSection = document.getElementById("answer-section"); //point to the answer button section
+    let answerButton = document.createElement("BUTTON");  
 
     //declare overarching function that will contain smaller nested functions that are each part of gameplay
     function playGame(){
@@ -46,37 +48,79 @@ let codeQuiz = {
         //DISPLAY THE QUESTION
         // find and display question number 
         document.getElementById("question-number").innerHTML =  questionNumber;
-        //point to the question feild
-        let questionField = document.getElementById("question-field");
+      
         questionField.innerHTML = codeQuiz.questionsAndAnswers[questionNumber - 1].question;
 
         //DISPLAY ANSWER BUTTONS
-        var answerSection = document.getElementById("answer-section"); //find the answer button section
         answerSection.innerHTML = ""; //clear the old stuff
 
         // a little loop that will append a button for each possible answer option in the array
         for (let i = 0; i < codeQuiz.questionsAndAnswers.length - 1; i++ ){
             console.log(codeQuiz.questionsAndAnswers[questionNumber-1].options[i]);
-
-            let answerButton = document.createElement("BUTTON");   
+ 
+            let answerButton = document.createElement("BUTTON");  
             answerButton.innerHTML = codeQuiz.questionsAndAnswers[questionNumber-1].options[i];                   
             answerSection.appendChild(answerButton);
 
+            //once the user clicks a answer button, call up the functino to check the answer
             answerButton.addEventListener("click", checkAnswer);
         };
     } 
 
 
     function checkAnswer(choice){
-        console.log("check answer");
+        console.log(choice);
 
-       
+        let noteSection = document.getElementById("note"); //find the answer button section
+        let userChoice = choice.srcElement.innerHTML;
 
+        //compare answer from codeQuiz object to the innerHTML of the inner HTML of the soucre element of the click event
+        if (userChoice === codeQuiz.questionsAndAnswers[questionNumber-1].rightAnswer){
+        
+            //display confirmation message
+            noteSection.innerHTML = "Yes! Your last answer was right! You've earned a point! "; 
+            score++; //increment score. 
+
+        } else {
+            noteSection.innerHTML = "Sorry! your last answer was incorrect. You've lost time on the clock. "; 
+        };
+
+        updateScore();
 
     };
 
     function updateScore() {
-        console.log("update score" );
+        console.log("update score");
+
+
+        //display score here. 
+
+        nextQuestion();
+
+    };
+
+    function nextQuestion() {
+        console.log("next question");
+
+        //if there are still more questions, increment the question number, and run the whole thing through again.  
+        if (questionNumber < codeQuiz.questionsAndAnswers.length){
+            questionNumber++; 
+            playGame();
+            
+        //if there are no more questions, prompt the user to play again
+        } else{
+            questionField.innerHTML = "Would you like to play again?"
+
+            let answerButton = document.createElement("BUTTON");  
+            answerButton.innerHTML = "Play Again!";
+            answerSection.innerHTML = "";
+            answerSection.appendChild(answerButton);
+
+            //once the user clicks to play again, call up the functiion to play again! 
+            answerButton.addEventListener("click", playGame);
+
+        }
+
 
     };
 
